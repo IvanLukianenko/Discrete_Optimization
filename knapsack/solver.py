877625efb6ab58cfc76ca50a8e6baf5b424cf9f0
IvanLukianenko <lukianenko.ia@phystech.edu>
2021-02-22 15:11:08 +0300

    BnB added

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -4,6 +4,32 @@
 from collections import namedtuple
 Item = namedtuple("Item", ['index', 'value', 'weight'])
 Item_2 = namedtuple("Item", ['index', 'value', 'weight', 'value_on_weight'])
+global_max = -1
+
+def LS(o_v, f_s, actl_v, tkn):
+    global global_max
+    #print("обработал вершину " + str(len(tkn)))
+    #print(o_v, global_max)
+    if (f_s < 0):
+        return -1, []
+    if (len(tkn) == n) and (actl_v < global_max):
+        return -1, []
+    else:
+        if (len(tkn) == n):
+            return actl_v, tkn
+    if (o_v < global_max):
+        return -1, tkn
+    if (o_v < 0):
+        return -1, tkn
+    a, b = LS(o_v, f_s - w[len(tkn)], actl_v + v[len(tkn)], tkn + [1])
+    a1, b1 = LS(o_v - v_w[len(tkn)], f_s, actl_v, tkn + [0])
+
+    if a > a1:
+        global_max, tkn = a, b
+    else:
+        global_max, tkn = a1, b1
+    return global_max, tkn
+
 def Traceback(arr, K, w):
     items = []
     j = len(w)
@@ -47,8 +73,8 @@ def greedy_algo(items_2, K):
             actual_weight += items_2[i].weight
             taken.append(items_2[i].index)
     del taken[-1]
-    return taken, actual_value - items_2[i].value
-
+    return taken, actual_value - items_2[-1].value
+    
 
 
 
@@ -70,30 +96,53 @@ def solve_it(input_data):
         line = lines[i]
         parts = line.split()
         items.append(Item(i-1, int(parts[0]), int(parts[1])))
-        items_2.append(Item_2(i-1, int(parts[0]), int(parts[1]), int(parts[0])/int(parts[1]) ) ) 
+        items_2.append(Item_2(i-1, int(parts[0]), int(parts[1]), float(float(parts[0])/float(parts[1])) ) ) 
 
 
     # a trivial algorithm for filling the knapsack
     # it takes items in-order until the knapsack is full
     
-    #value = 0
-    #weight = 0
+    value = 0
+    weight = 0
     taken = [0]*len(items)
 
+    global v 
     v = [item.value for item in items]
+    global w 
     w = [item.weight for item in items]
     K = capacity
+    global v_w
+    v_w = [item.value_on_weight for item in items_2]
+    global n
+    n = item_count
+    o_v = 0.0
+    res_weight = 0
+    items_sor = sorted(items_2, key = lambda item : item.value_on_weight, reverse = True)
+    for item in items_sor:
+        o_v += item.value
+        res_weight += item.weight
+        if res_weight > K:
+            index_val = item.index
+            o_v = o_v - item.value
+            res_weight -= item.weight
+            o_v = o_v + float((K - res_weight) * item.value_on_weight)
+            #print(item.value_on_weight)
+            break
+    #print(o_v)
+    #o_v = sum (v)
+    value, taken = LS(o_v, K, 0, [])
+
     #print(w)
-    if len(items)*K < 1e9:
-        table = create_table(v,w,K)
-        value = table[-1][-1]
-        taken_1 = Traceback(table, K, w)
-        for i in taken_1:
-            taken[i-1] = 1
-    else: 
-        taken_2, value= greedy_algo(items_2, K)
-        for i in taken_2:
-            taken[i-1] = 1
+    #if len(items)*K < 1e9:
+    #    table = create_table(v,w,K)
+    #    value = table[-1][-1]
+    #    taken_1 = Traceback(table, K, w)
+    #    for i in taken_1:
+    #        taken[i-1] = 1
+    #else: 
+    #    taken_2, value= greedy_algo(items_2, K)
+    #    for i in taken_2:
+    #        taken[i-1] = 1
     #print(table)
     #for item in items:
     #    if weight + item.weight <= capacity:
