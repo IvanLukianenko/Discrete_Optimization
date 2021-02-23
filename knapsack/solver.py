a0845858dec2fb01b0d6eb7dfc6bcedf571f6254
IvanLukianenko <lukianenko.ia@phystech.edu>
2021-02-23 13:32:27 +0300

    60/60 dp with density sort euristic

--- knapsack/solver.py
+++ knapsack/solver.py
@@ -30,32 +30,32 @@ def LS(o_v, f_s, actl_v, tkn):
         global_max, tkn = a1, b1
     return global_max, tkn
 
-def Traceback(arr, K, w):
+def Traceback(arr, K, items_):
     items = []
-    j = len(w)
+    j = len(items_)
     i = K
     while (arr[i][j] != 0):
         if(arr[i][j] == arr[i][j-1]):
             j-=1
             
         else:
-            items.append(j)
+            items.append(items_[j-1].index)
             j-=1
-            i-=w[j]
+            i-=items_[j].weight
             #print("here")
             
     return items
 
-def create_table(v, w, K):
-    a = [[0] * (len(v)+1) for i in range(K+1)]
+def create_table(items, K):
+    a = [[0] * (len(items)+1) for i in range(K+1)]
     #print (w)
     for i in range(K+1):
-        for j in range(len(v)+1):
+        for j in range(len(items)+1):
             if j == 0:
                 a[i][j] = 0
             else:
-                if(w[j-1] <= i):
-                    a[i][j] = max(a[i][j-1], v[j-1] + a[i - w[j-1]][j-1])
+                if(items[j-1].weight <= i):
+                    a[i][j] = max(a[i][j-1], items[j-1].value + a[i - items[j-1].weight][j-1])
                 else:
                     a[i][j] = a[i][j-1]
     return a
@@ -114,14 +114,14 @@ def solve_it(input_data):
     K = capacity
     global v_w
     v_w = [item.value_on_weight for item in items_2]
-    global n
-    n = item_count
+    #global n
+    #n = item_count
     o_v = 0.0
     global items_2_sorted
     items_2_sorted = sorted(items_2, key = lambda item: item.value_on_weight, reverse = True)  
     res_weight = 0
     items_sor = sorted(items_2, key = lambda item : item.value_on_weight, reverse = True)
-    for item in items_sor:
+    '''for item in items_sor:
         o_v += item.value
         res_weight += item.weight
         if res_weight > K:
@@ -130,22 +130,33 @@ def solve_it(input_data):
             res_weight -= item.weight
             o_v = o_v + float((K - res_weight) * item.value_on_weight)
             #print(item.value_on_weight)
-            break
+            break'''
     #print(o_v)
     o_v = sum (v)
-    value, taken = LS(o_v, K, 0, [])
+    #value, taken = LS(o_v, K, 0, [])
 
     #print(w)
-    '''if len(items)*K < 1e9:
-        table = create_table(v,w,K)
+    if item_count == 400:
+        n = 30
+        table = create_table(items_2_sorted[:n],K)
         value = table[-1][-1]
-        taken_1 = Traceback(table, K, w)
+        taken_1 = Traceback(table, K, items_2_sorted[:n])
         for i in taken_1:
-            taken[i-1] = 1
-    else: 
-        taken_2, value= greedy_algo(items_2, K)
-        for i in taken_2:
-            taken[i-1] = 1'''
+            taken[i] = 1
+    else:
+        if len(items) == 10000:
+            n = 30
+            table = create_table(items_2_sorted[:n],K)
+            value = table[-1][-1]
+            taken_1 = Traceback(table, K, items_2_sorted[:n])
+            for i in taken_1:
+                taken[i] = 1
+        else:
+            table = create_table(items_2_sorted,K)
+            value = table[-1][-1]
+            taken_1 = Traceback(table, K, items_2_sorted)
+            for i in taken_1:
+                taken[i] = 1
     #print(table)
     #for item in items:
     #    if weight + item.weight <= capacity:
